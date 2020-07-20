c5862b7b529b1fd55d5e9af95b4516ddfcd5aade
unknown <ponggas2@gmail.com>
2020-07-20 15:39:25 +0900

    회원가입 로그인

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,44 +1,80 @@
-let index = { //여기서는 이벤트 리스너를 바인딩만 하고
+let index = { // 여기서는 이벤트 리스너를 바인딩만 하고
 	init : function() {
 		
-		$("#btn-save").on("click", ()=> {
-			this.save();
+		$("#btn-save").on("click", ()=> { // 리스너
+			this.save(); // 콜백
 		});
 		
-		$("#btn-update").on("click", ()=> {
-			this.update();
+		$("#btn-login").on("click", ()=> { // 리스너
+			this.login(); // 콜백
 		});
+		
 		
 	},
 	
 	save: function() { // 실제 로직은 여기서 실행
-		alert("btn-save 로직 실행");
+		
 		let data ={
 				username : $("#username").val(),
 				password : $("#password").val(),
 				email : $("#email").val()
 		};
-		alert("btn-save data");
 		
 		$.ajax({ // 공식!
 			
 			type:"post",
 			url: "/auth/joinProc",
 			data: JSON.stringify(data),
-			contentType: "application/json; charset=utf-8",  //http에서는 Content-Type 라고 씀
+			contentType: "application/json; charset=utf-8",  // http에서는
+																// Content-Type
+																// 라고 씀
 			dataType: "json"  // 스프링은 응답할때 스트링일 경우 무조건 json으로 응답한다
 			
 		}).done((resp)=>{
-			console.log(resp);
+			
+			alert("회원가입 성공");
+			location.href="/";
+			
 		}).fail((error)=>{
+			
+			alert("회원가입 실패");
 			console.log(error);
+			
 		});
 		
 	},
 	
-	update: function() { // 실제 로직은 여기서 실행
-		alert("btn-update 로직 실행");
+	login : function() { // 실제 로직은 여기서 실행
+		
+		let data ={
+				username : $("#username").val(),
+				password : $("#password").val()
+		};
+		
+		$.ajax({ // 공식!
+			
+			type:"post", // 아이디 패스워드는 포스트로!
+			url: "/auth/loginProc",
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",  // http에서는
+																// Content-Type
+																// 라고 씀
+			dataType: "json"  // 스프링은 응답할때 스트링일 경우 무조건 json으로 응답한다
+			
+		}).done((resp)=>{
+			
+			alert("로그인 성공");
+			location.href="/";
+			
+		}).fail((error)=>{
+			
+			alert("로그인 실패");
+			console.log(error);
+			
+		});
+		
 	}
+
 }
 
 index.init();
